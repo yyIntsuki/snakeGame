b2b7a0d2ebc56c98e3193fc02ccdb5ea68939bdf
yyIntsuki <intsuki.yz@gmail.com>
2018-12-03 13:22:08 +0100

    Attempt 2 to make the game work using Heroku.

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var sitePath = process.argv[2] || ".";
-var port = 4242;
 
 var express = require('express');
 var app = express();
@@ -12,6 +11,9 @@ app.use(function(req, res, next) {
 console.log(sitePath);
 console.log("Starting server in: " + __dirname + '/' + sitePath);
 app.use(express.static(__dirname + '/' + sitePath));
-app.listen(port, function() { 
-    console.log("Server running at: http://localhost:" + port)
+var PORT = process.env.PORT || 4242;
+
+//Start the web server!
+app.listen(PORT, function(){
+    console.log('Server up and running');
 });
